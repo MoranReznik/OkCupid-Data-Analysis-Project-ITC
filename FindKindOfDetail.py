70871b9237f64857ca5ae1e6c07527577c389f6a
moran <moranreznik@gmail.com>
2019-11-28 11:00:19 +0200

    fixed comment from first checkpoint redo: 1. all functions are imported using "." notation instead of  "from" notation in order to prevent confusion for function names. 2. created a separate file for all global variables called GV. 3. the dictionary containing the labels for the different profile data categories is now a json file. 4. all of the code is up to PEP8 standards. 5. all of the docstring are formatted numpy style and include input output explanations. 6. "scrape_profile" function refactored to be shorter and call another new function to handle parsing the data. 7. all the OkCupid data is now encrypted. Please note that it is currently printed to the console as is - but it will be entered the database encrypted.

--- FindKindOfDetail.py
+++ FindKindOfDetail.py
@@ -1,11 +1,26 @@
-import DictData
+import json
+
+with open('choices.json') as json_file:
+    DICTDATA = json.load(json_file)
 
 def find_kind_of_detail(string):
-    """helper func for the scrape func. in order to place a single detail on a person
+    """ helper func for the scrape func. in order to place a single detail on a person
     in the right key, we need to know to what category this detail belongs to
-    possible categories are specified in the scrape func's docstring"""
+    possible categories are specified in the scrape func's docstring
 
-    for key, values in DictData.choices.items():
+            Parameters
+            ----------
+            string : string
+                the detail of which we want know the category of
+
+
+            Returns
+            -------
+            key : string
+                the category of the input detail
+    """
+
+    for key, values in DICTDATA.items():
         for value in values:
             if value.lower() in string.lower():
                 return key
---
+++ Main.py
@@ -0,0 +1,50 @@
+import EnterProfile
+import ScrapeProfile as scrape
+import SiteLogin
+import WaitForProfileToLoad
+import GV
+import logging
+import json
+from cryptography.fernet import Fernet
+
+
+# privacy :  in order to encrypt the data to protects privacy of OkCupid Users, all the data in the
+#      database will be encrypted.
+#      the key will only be saved in a separate file in the repo, so even if the database will go to the
+#      wrong hands, it will remain safe.
+file = open('OkCupidKey', 'rb')  # opening the key file
+key = file.read()
+cipher = Fernet(key)
+file.close
+
+
+def main():
+
+    counter = 0
+
+    for prof_name, login_details in GV.PROFILES.items():
+        driver = SiteLogin.site_login(login_details)
+        for i in range(1, GV.NUMBER + 1):
+            try:  # in case the program crushes, restart and complete the task
+                print("%s: page number %s" % (prof_name, str(i)))
+                profile_driver_unloaded, num_pics = EnterProfile.enter_profile(driver)
+                profile_driver_loaded = WaitForProfileToLoad.wait_for_profile_to_load(profile_driver_unloaded)
+                driver, profile_data = scrape.scrape_profile(profile_driver_loaded, num_pics)
+                profile_data_byte = json.dumps(profile_data).encode('utf-8')
+                encrypted_data = cipher.encrypt(profile_data_byte)
+                GV.PROFILES[prof_name]["data"][i] = encrypted_data
+                counter += 1
+
+            except Exception as e:
+                print('problem with loading the data. moving to the next person.')
+                driver.close()
+                driver = SiteLogin.site_login(login_details)
+                logging.exception(e)
+
+        driver.close()
+
+    print('extracted data from ' + str(counter)+' out of ' + str(GV.NUMBER * len(GV.PROFILES)) + ' profiles attempted')
+
+
+if __name__ == '__main__':
+    main()
