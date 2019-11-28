47651f906d761c8ce51c4bdde136039e05859356
moran <moranreznik@gmail.com>
2019-11-28 11:43:46 +0200

    removed files

--- FindKindOfDetail.py
+++ FindKindOfDetail.py
@@ -3,6 +3,7 @@ import json
 with open('choices.json') as json_file:
     DICTDATA = json.load(json_file)
 
+
 def find_kind_of_detail(string):
     """ helper func for the scrape func. in order to place a single detail on a person
     in the right key, we need to know to what category this detail belongs to
