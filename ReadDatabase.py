7b0ad51feefc16aa746eae74ccb1fc9339e9e3f2
Gidon Weil <Gidon.we@gmail.com>
2019-12-03 23:29:26 +0200

    fixes for reading and updating the DB

--- ReadDatabase.py
+++ ReadDatabase.py
@@ -3,9 +3,23 @@ import GV
 import pandas as pd
 
 
-def update_database(information, conditions):
+def read_database(information, conditions):
+    """ Reads from the database all the wanted information according to the wanted conditions
+
+            Parameters
+            ----------
+            information : list
+                wanted categories to output
+            conditions : dictionary
+                according to which conditions to output
+
+
+            Returns
+            -------
+            df : Pandas DataFrame
+                DataFrame consisting of wanted categories according to the conditions
     """
-    """
+
     con = mysql.connector.connect(host='localhost', user=GV.MYSQL_USERNAME, passwd=GV.MYSQL_PASSWORD,
                                   auth_plugin='mysql_native_password')
     cur = con.cursor()
@@ -13,86 +27,57 @@ def update_database(information, conditions):
 
     # SELECT columns
     select = 'SELECT '
-    for info in information:
+    all_information = information.copy()
+    if information == ['all']:  # Extract the list of all categories to output
+        cur.execute("SHOW TABLES")
+        categories = cur.fetchall()
+        all_information = ['age', 'height', 'location', 'num_pics']
+        for category in categories:
+            all_information += [category[0]]
+        all_information.remove('profiles')
+
+    for category in all_information:
         if select != 'SELECT ':
             select += ', '
-        if info in ['Age', 'Num_pics']:
-            insert = (info, )
-            select += 'profiles.%s' % insert
+        if category in ['age', 'height', 'location', 'num_pics']:
+            select += 'profiles.%s' % (category, )
         else:
-            insert = (info, info)
-            select += '%s.%s' % insert
+            select += '%s.%s' % (category, category)
 
-    # FROM tables
-    tables = set(information + list(conditions.keys()))
-    '''LEFT JOIN body_type ON profiles.main_id = body_type.main_id'''
+    # JOIN ON
     join = ' FROM profiles '
+    tables = set(all_information + list(conditions.keys()))
     for table in tables:
-        if table not in ['Age', 'Num_pics']:
+        if table not in ['age', 'height', 'location', 'num_pics']:
             join += "LEFT JOIN %s ON profiles.main_id = %s.main_id " % (table, table)
 
-    # WHERE conditions
-    where = ' WHERE '
-    for parameter, condition in conditions.items():
-        if where != ' WHERE ':
-            where += ' AND '
-        if parameter in ['Age', 'Num_pics']:
-            insert = (parameter, condition[0], condition[1])
-            where += '(profiles.%s BETWEEN %s AND %s)' % insert
-        else:
-            where += '('
-            for cond in condition:
-                insert = (parameter, parameter, cond)
-                where += '%s.%s="%s" OR ' % insert
-            where = where[:-4] + ')'
+    # WHERE condition
+    if not conditions:
+        where = ''
+    else:
+        where = ' WHERE '
+        for category, condition in conditions.items():
+            if where != ' WHERE ':
+                where += ' AND '
+            if category in ['age', 'num_pics']:
+                where += '(profiles.%s BETWEEN %s AND %s)' % (category, condition[0], condition[1])
+            else:
+                where += '('
+                for cond in condition:
+                    where += '%s.%s="%s" OR ' % (category, category, cond)
+                where = where[:-4] + ')'
 
     sql = select + join + where
-    print(sql)
-
-
-    #
-    # #
-    # sql = '''SELECT profiles.main_id, profiles.Age, Tobacco.Tobacco
-    # FROM profiles
-    # LEFT JOIN body_type ON profiles.main_id = body_type.main_id
-    # LEFT JOIN diet ON profiles.main_id = diet.main_id
-    # LEFT JOIN drinks ON profiles.main_id = drinks.main_id
-    # LEFT JOIN tobacco ON profiles.main_id = tobacco.main_id
-    #
-    # WHERE profiles.Age BETWEEN 20 AND 23'''
-
-    #
-    # sql = """
-    # SELECT Gender.Gender, profiles.Age, Tobacco.Tobacco
-    # FROM profiles
-    # LEFT JOIN Tobacco ON profiles.main_id = Tobacco.main_id
-    # LEFT JOIN Gender ON profiles.main_id = Gender.main_id
-    # WHERE (Gender.Gender="Male" OR Gender.Gender="Woman") AND (profiles.Age BETWEEN 5 AND 100)
-    # """
-
-
-    cur.execute(sql)
-    data = cur.fetchall()
-    print(data)
-    #
-    #
-    #
-    #
-    # df = pd.read_sql(sql, con)
-    # print(df)
-
+    df = pd.read_sql(sql, con)
     con.close()
+    return df
 
 
-information = ['Gender', 'Age', 'Tobacco']
-conditions = {'Gender': ['Man', 'Woman'], 'Age': ['20', '30']}
-update_database(information, conditions)
 
-# sql = '''SELECT profiles.main_id, profiles.Age, Tobacco.Tobacco FROM
-# body_type, diet, drinks,
-# gender, profiles, tobacco
-# WHERE profiles.Age BETWEEN 20 AND 30'''
-# print(sql)
-# cur.execute(sql)
-# data = cur.fetchall()
-# print(data)
+info = ['gender', 'age', 'tobacco', 'drugs', 'num_pics']
+info = ['all']
+cond = {}
+
+cond = {'gender': ['man', 'woman'], 'age': ['20', '30'], 'tobacco': ["doesn't smoke cigarettes"]}
+df = read_database(info, cond)
+print(df)
--- UpdateDatabase.py
+++ UpdateDatabase.py
@@ -2,21 +2,18 @@ import mysql.connector
 import GV
 
 
-def update_database(profile_data, scraper_profile_name):
+def update_database(profile_data):
     """ Updating MySQL database with all the date we scraped.
     The profile_id is encrypted to protects privacy of OkCupid Users.
     the key will be saved in a separate file in the repo, so even if the database will go to the
     wrong hands, it will remain safe.
-    If the profile already exists in the database, since the details might have been updated,
-    delete it from the database and re-add it.
+    If the profile already exists in the database, add all new and updated data to the database.
 
             Parameters
             ----------
             profile_data : dictionary
                 dictionary with all of the scraped data from the profile.
 
-            scraper_profile_name : str
-                the name of the current scraping profile (specified in the GV file)
     """
 
     con = mysql.connector.connect(
@@ -29,44 +26,31 @@ def update_database(profile_data, scraper_profile_name):
     profile_id = profile_data['Profile_id']
 
     # check if there is a duplicate profile_id
-    insert = (profile_id, key)
-    sql = '''SELECT main_id, COUNT(Profile_id) from profiles WHERE Profile_id=AES_ENCRYPT("%s","%s")''' % insert
+    sql = '''SELECT main_id, COUNT(Profile_id) from profiles WHERE Profile_id=AES_ENCRYPT("%s","%s")''' % \
+          (profile_id, key)
     cur.execute(sql)
     main_id, exists = cur.fetchone()
 
-    # if there is a duplicate, remove its data from profiles table after removing it from the rest of the the tables
-    if exists:
-        cur.execute("SHOW TABLES")
-        tables = cur.fetchall()
-        tables.append(tables.pop(tables.index(('profiles',))))  # move profiles to the end of the list
-        for table in tables:
-            sql = ''' DELETE FROM %s WHERE main_id="%s" ''' % (table[0], main_id)
-            cur.execute(sql)
+    # first: if profile_id does't exist: add the encrypted unique profile_id to the profiles table and extract the PK
+    if not exists:
+        sql = '''INSERT INTO profiles (Profile_id) VALUES (AES_ENCRYPT("%s","%s"))''' % (profile_id, key)
+        cur.execute(sql)
 
-    # first: add the encrypted unique profile_id and the main_profile_name to the profiles table and extract the PK
-    insert = (profile_id, key, scraper_profile_name)
-    sql = '''INSERT INTO profiles (Profile_id, Scraper_profile) VALUES (AES_ENCRYPT("%s","%s"), "%s")''' % insert
-    cur.execute(sql)
+        cur.execute('SELECT LAST_INSERT_ID()')
+        main_id = cur.fetchone()[0]
 
-    cur.execute('SELECT LAST_INSERT_ID()')
-    main_id = cur.fetchone()[0]
-
-    for column, value in profile_data.items():
-
+    for column, values in profile_data.items():
         # next: add the details relevant to the profiles table
         if column in ['Age', 'Height', 'Location', 'Num_pics']:
-            insert = (column, value, main_id)
-            sql = ''' UPDATE profiles SET %s = "%s" WHERE main_id = "%s" ''' % insert
+            sql = ''' UPDATE profiles SET %s = "%s" WHERE main_id = "%s" ''' % (column, values, main_id)
             cur.execute(sql)
 
         # finally: add the rest of the data to their table, where some of the data are str and some are lists of str.
         elif column != 'Profile_id':
-            values = value
-            if type(value) == str:
-                values = [value]
-            for val in values:
-                insert = (column, column, main_id, val)
-                sql = '''INSERT INTO %s (main_id, %s) VALUES ("%s", "%s")''' % insert
+            if type(values) == str:
+                values = [values]
+            for value in values:
+                sql = '''INSERT INTO %s (main_id, %s) VALUES ("%s", "%s")''' % (column, column, main_id, value)
                 cur.execute(sql)
 
     con.commit()
